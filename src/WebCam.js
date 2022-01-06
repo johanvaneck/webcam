0f3633ed7346ba9bb84364c59313491c1e5fc012
johanvaneck <jve.software@gmail.com>
2022-01-06 20:52:29 +0200

    second commit

--- src/WebCam.js
+++ src/WebCam.js
@@ -3,6 +3,13 @@ import Webcam from 'react-webcam';
 
 export class MyWebCam extends React.Component{
     render(){
-        return <Webcam />;
+        return (
+        <div>
+            <h1>Welcome to my webcam!</h1>
+            <p>The webcam should show below this.</p>
+            <Webcam />;
+            <p>The webcam should show above this.</p> 
+        </div>
+        );
     }
 }
